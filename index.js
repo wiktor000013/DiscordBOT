b01c3849105245c043364ce55c8a91c5d08e9f56
wiktor000013 <74265533+wiktor000013@users.noreply.github.com>
2021-01-22 18:58:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -86,10 +86,10 @@ channel.setName('Wakacje: ' + days);
 }, 1000);
 client.on('message', message => {
 
-        if (message.channel.id === 802115960432492555) {
+        if (message.channel.id == 802115960432492555) ){
 		if (message.content.startsWith('https://www.you'){
-		message.reply("Pomyliłeś kanały');
-		    return;
+			message.reply("Pomyliłeś kanały');
+		   	return;
 		    }
         }
 
