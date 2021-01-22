3ed61e827acb8f172056d29170d1bc034e23ba70
wiktor000013 <74265533+wiktor000013@users.noreply.github.com>
2021-01-22 19:22:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -102,11 +102,9 @@ client.on('message', message => {
 setInterval(function(){
 	client.on('message', message => {
    if (message.channel.id == 802115960432492555){
-	if( message.content.startsWith('Queued'))
-		{
-			message.delete();
-		}
-   }
+if(message.author.bot){
+	message.delete()
+}
 	});
 },1000);
 client.on('message', message => {
