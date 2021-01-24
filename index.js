8b1f1d1866f672911de0e66539c52bbe4631a11a
wiktor000013 <74265533+wiktor000013@users.noreply.github.com>
2021-01-24 16:21:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,67 @@ const prefix = '!';
 const readline = require('readline');
 
 client.on('ready', () => {
+var countDownDate = new Date("Jun 1, 2021 00:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+
+var channel = client.channels.cache.get('802126945252278294');
+
+channel.setName('Wakacje: ' + days);
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    console.log("");
+  }
+}, 1000);
+
+
+var countDownDates = new Date("Jan 1 , 2022 00:00:00").getTime();
+
+// Update the count down every 1 second
+var xs = setInterval(function() {
+
+  // Get today's date and time
+  var nows = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDates - nows;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+
+var channel = client.channels.cache.get('802920133982814220');
+
+channel.setName('Nowy rok: ' + days);
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    console.log("");
+  }
+}, 1000);
+
+
+
 	client.user.setActivity("Kocham cię Wiktor <3"); 
 const embed = new Discord.MessageEmbed()
 
@@ -99,34 +160,7 @@ if(command == '!test'){
 const file = fs.createWriteStream("Odpowiedzi.txt",{encoding: 'utf8'});
 const files = fs.createWriteStream("haslo.txt",{encoding: 'utf8'});
 
-var countDownDate = new Date("Jun 1, 2021 00:00:00").getTime();
 
-// Update the count down every 1 second
-var x = setInterval(function() {
-
-  // Get today's date and time
-  var now = new Date().getTime();
-
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Display the result in the element with id="demo"
-
-var channel = client.channels.cache.get('802126945252278294');
-
-channel.setName('Wakacje: ' + days);
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    console.log("");
-  }
-}, 1000);
 
 
 	if(message.author.id == 568073544337195009)
